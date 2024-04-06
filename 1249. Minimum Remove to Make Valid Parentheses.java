2a58a753ff1683c050abc8a3cd96298ed795c217
Divyanshu Shukla <divyanshushukla82@gmail.com>
2024-04-06 12:05:06 +0530

    6th April 2024

--- 1249. Minimum Remove to Make Valid Parentheses.java
+++ 1249. Minimum Remove to Make Valid Parentheses.java
@@ -1,4 +1,4 @@
-/*Given a string s of '(' , ')' and lowercase English characters.
+/*Given a string s of '(' , ')' and lowercase English characters
 
 Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
 
